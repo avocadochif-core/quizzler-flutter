d573531f8325e1031818eab501ec902ba783093c
Stepan Revytskyi <avocadochif@gmail.com>
2020-06-15 23:12:07 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, statusBarBrightness: Brightness.light));
+
+  runApp(Quizzler());
+}
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
